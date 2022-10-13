111385da97b5bbc680ec335241908652f61c206d
mucarb <murilo.rb60@gmail.com>
2022-10-13 16:23:50 -0300

    Installing and importing tailwind postcss autoprefixer libraries

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@
  * 1º npm create vite@latest
  * 2º npm install
  * 3º npm run dev (executa o programa)
+ * 4º npm install -D tailwindcss postcss autoprefixer
+ * 5º npx tailwindcss init -p (-p cria o cria tambem o arquivo de configuração do postcss)
  */
 
+import "./styles/global.css";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  // classes de estilização importadas do tailwind
+  return (
+    <>
+      <h1 className="font-bold text-5xl text-violet-500">Hello World!</h1>
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  );
 }
