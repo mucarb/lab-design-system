702cec871a9ff216734b121c19da99b748a22be0
mucarb <murilo.rb60@gmail.com>
2022-10-13 20:12:54 -0300

    Importing Figma design colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#a7e3f9',
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
